c3410815f4c2e821a1405b9f5a7709df07ac914b
NataliZero <ball.diamond.ru@gmail.com>
2024-09-18 19:43:02 +0300

    add new const

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIGTH = 800
+SCREEN_HEIGHT = 600
+screnn = pygame.display.set_mode((SCREEN_WIGTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load("")
+
+running = True
+while running:
+    pass
+pygame.quit()
+
+
