fceab3ced236cb795c13f0582c457cc73a16c587
NataliZero <ball.diamond.ru@gmail.com>
2024-09-18 22:17:29 +0300

    add all const
     2 фото, аватар и цель

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 pygame.init()
 
 SCREEN_WIGTH = 800
@@ -7,7 +7,19 @@ SCREEN_HEIGHT = 600
 screnn = pygame.display.set_mode((SCREEN_WIGTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("")
+icon = pygame.image.load("img/тир.PNG")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/target.png")
+target_width = 80
+target_height = 80
+
+target_x = random.randint(0, SCREEN_WIGTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
 
 running = True
 while running:
