0dc86888b5cfc27142b99318b758cf3a0f179efc
NataliZero <ball.diamond.ru@gmail.com>
2024-09-19 11:48:16 +0300

    add all cyccle code
    Игра написана

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
 import random
+
 pygame.init()
 
 SCREEN_WIGTH = 800
 SCREEN_HEIGHT = 600
-screnn = pygame.display.set_mode((SCREEN_WIGTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIGTH, SCREEN_HEIGHT))  # Исправлено
 
 pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/тир.PNG")
@@ -19,11 +20,23 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-
-
 running = True
 while running:
-    pass
+    screen.fill(color)  # Используем правильную переменную 'screen'
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            # Исправлено условие для клика по мишени
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIGTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+    screen.blit(target_img, (target_x, target_y))
+
+    pygame.display.update()
+
 pygame.quit()
 
 
